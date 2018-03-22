fb0c2beb14953c7d5d8b8af35c6ee16c03bc61d5
ejames9 <foster9@icloud.com>
2018-03-22 11:25:48 -0700

    Minor fixes to index.js

--- index.js
+++ index.js
@@ -1,103 +1,191 @@
 /*
-gulp-elementsJS-interpreter,  gulp-plugin
+gulp-autopatch,  gulp-plugin
 v0.1.0
 
 Author: Eric James Foster
 License: MIT
 */
 
-var Transform = require('stream').Transform,
-           fs = require('fs'),
-         util = require('util'),
-          vfs = require('vinyl-fs'),
-          log = require('elementsJS').log,
-        gutil = require('gulp-util'),
-     streamer = require('event-stream').through,
-        _____ = require('_____'),
-
-  PluginError = gutil.PluginError;
+const fs = require('fs'),
+     log = require('elementsJS').log,
+    info = require('elementsJS').info,
+     err = require('elementsJS').err
 
 
+// The plugin's name...
+const PLUGIN_NAME = 'gulp-npmAutopatch'
+// Get node's child_process spawner....
+const exec = require('child_process').exec
 
-const PLUGIN_NAME = '';
 
+//The PLugin
+function npmAutopatch(packageRoot) {
+  var rePublish,
+  splitVersion,
+  unSplitVersion,
+  patchedArray
 
+// Get patch limit from package.json
+  const packJSONString = fs.readFileSync(packageRoot + 'package.json')
 
-//PLugin
-function gulp_____Plugin() {
-  /*If the interpreter is not instantiated by having called the function with the "new" keyword, return
-  a "new" instance of the class.*/
-  if (!(this instanceof gulp_____Plugin)) {
-    return new gulp_____Plugin();
+  const packageJSON = JSON.parse(packJSONString),
+         patchLimit = packageJSON.patchLimit || 0
+
+// Store shell command args...
+  const minorArgs = [
+    'version',
+    'minor'
+  ]
+
+  const patchArgs = [
+    'version',
+    'patch'
+  ]
+
+// store root...
+  const options = {
+    cwd: packageRoot
   }
-  //This is a trick that applies the internal properties of the Transform class to my interpreter class.
-  Transform.call(this, {objectMode: true} );
 
-  //Internal plugin function, does the actual transformation of files.
-  this._stream = ()=> {
-    var s = streamer(function write(data) {
-      data = _____(data);
-      this.emit('data', data);
-    });
-    return s;
-  };
+// Get current version...
+  const version = packageJSON.version
 
-  /*Create stream for files to pass through.
-  Define _transform method as required by node for Transform streams*/
-  this._transform = (file, encoding, fin)=> {
-    // console.dir(file);
-    //If file is Vinyl object, continue to transformation.
-    if (file) {
-      //log('file is Vinyl', ['magentaBright', 'bold']);
-      //If file is null, emit PluginError.
-      if (file.isNull()) {
-        this.emit('error', new PluginError(PLUGIN_NAME, 'File is Null'));
-        fin();
+// a function for splitting the version number into an array of 3 numbers...
+  splitVersion =(version)=>
+    version.split('.')
+
+// a boolean property used to determine if update is patch or minor...
+  const patch = splitVersion(version)[2] < patchLimit || patchLimit === 0
+
+// a function for splitting the version number into an array of 3 numbers...
+  unSplitVersion =(versionArr)=>
+    versionArr.join('.')
+
+// Given a string version number array, returns a patch incremented string...
+  patchedArray =(versionArr)=>
+    versionArr.map((num, ix)=>
+      (ix === 2)?
+        num++
+      :
+        num
+    )
+
+  rePublish =()=>
+// Spawning another child process to re-publish the package, now that the version
+// has been incremented...
+    exec(
+     'npm publish',
+      options,
+      (error, stdout, stderr)=> {
+        if (error) {
+          err(`There was an error republishing ${packageJSON.name}`)
+          log(`${error}`, 'red')
+        } else {
+          info(`${packageJSON.name} was republished. \n\n Update Complete.`)
+          log(stdout, 'green')
+        }
       }
-      //If file is buffer, transform it, and push it back to the stream.
-      if (file.isBuffer()) {
-        // log('file is Buffer', ['cyanBright', 'bold']);
-        // log(file.contents, 'cyanBright');
+    )
 
-        //Transform file.contents, convert back to a buffer, and push it back to stream.
-        this.file = file.contents;
-        this.file = _____(this.file);
-        file.contents = new Buffer(this.file);
+// Narrative user updating...
+  info(`\nPackage '${packageJSON.name}' has a current version of ${packageJSON.version}.\n`)
+  info(
+   `Bumping to ${
+      unSplitVersion(
+        patchedArray(
+          splitVersion(
+            packageJSON
+                  .version
+          )
+        )
+      )
+    }`
+  )
 
-      //If file is stream, pipe it through internal transform function.
-      } else if (file.isStream()) {
-        // log('file is Stream', ['cyanBright', 'bold']);
-        // log(file.contents, 'cyanBright');
-
-        //Define the transformation stream.
-        this._transformStream = this._stream();
-        //Catch errors from the stream and emit a gulp PluginError.
-        this._transformStream.on('error', this.emit.bind(this, 'error'));
-        //Transform the contents.
-        file.contents = file.contents.pipe(this._transformStream);
-
+/* Begin process of incrementing the version number and replublishing the package.
+** Use node's exec function to create a child process and execute unix commands.
+** We are diving a bit deeply into callback hell here, may want to re-write this
+** in the future with promises or async/await. It just doesn't matter at the
+** moment...
+**/
+  exec(
+// The patch bumping command...
+   `npm ${(patch ? patchArgs : minorArgs).join(' ')}`,
+    options,
+    (error, stdout, stderr)=> {
+      if (error) {
+// Log out error message...
+        err(
+          `There was an error bumping the version number of ${packageJSON.name}.`
+        )
+// Log out error...
+        log(`Error: ${error}`, 'red')
+// Try cleaning the directory...
+        info(
+          'Attempting to clean the directory.'
+        )
+        exec('git add .',
+          options,
+          (error, stdout, stderr)=> {
+// If there is an error, log it out...
+            if (error) {
+               log(`${error}`, 'red')
+// If no error, log stdout, and run next command...
+            } else {
+              log(stdout)
+              exec(
+               'git commit -m "Autocleaning directory."',
+                options,
+                (error, stdout, stderr)=> {
+// If there is an error, log it out...
+                  if (error) {
+                    log(`${error}`, 'red')
+// If no error, log stdout, and run next command...
+                  } else {
+                    log(stdout)
+// Run the patch bumping command one more time...
+                    exec(
+                     `npm ${(patch ? patchArgs : minorArgs).join(' ')}`,
+                      options,
+                      (error, stdout, stderr)=> {
+                        if (error) {
+// Log out error message...
+                          err(
+                            `There was an error bumping the version number of ${packageJSON.name}.`
+                          )
+                        } else {
+// Republish................................
+                          rePublish()
+                        }
+                      }
+                    )
+                  }
+                }
+              )
+            }
+          }
+        )
+      } else {
+// Narrative user update...
+        if (patch) {
+          info(`\n...............\n${packageJSON.name}'s version "patch" parameter was incremented by one.\n ...............`)
+        } else {
+          info(`\n...............\n${packageJSON.name}'s version "minor" parameter was incremented by one.\n ...............`)
+        }
+// Republish...
+        rePublish()
       }
-    //If file is neither buffer, stream or null Vinyl file object, emit error.
-    } else {
-      this.emit('error', new PluginError(PLUGIN_NAME, 'Invalid File Type'));
-      fin();
     }
-    //Push file to next plugin.
-    this.push(file);
-    //Tell stream engine we are done with file.
-    fin();
-  };
+  )
 }
 
-//This will apply the internal "methods" of the Transform class to my interpreter.
-util.inherits(gulp_____Plugin, Transform);
 
 
 
-module.exports = gulp_____Plugin;
+module.exports = npmAutopatch
 
 
 
 // vfs.src('./lib/test/IO/TestInput.js', {buffer: false})
-// .pipe(gulp_____Plugin())
+// .pipe(gulpAutoPatch())
 // .pipe(vfs.dest('./lib/test/'));
